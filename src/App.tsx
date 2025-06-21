fa3b7a52c1a666b5c2e83c1ddcff91740335dd3b
donghhan <apensia914@gmail.com>
2025-06-21 18:52:28 +0700

    Responsive HeroSection added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import style from "./App.module.css";
 import Header from "./components/sections/header/Header";
+import HeroSection from "./components/sections/hero/HeroSection";
 
 export default function App() {
   return (
     <div className={style.container}>
       <Header />
+      <HeroSection />
     </div>
   );
 }
--- src/components/sections/hero/HeroSection.tsx
+++ src/components/sections/hero/HeroSection.tsx
@@ -1,5 +1,24 @@
 import style from "./HeroSection.module.css";
+import heroImg from "../../../assets/images/hero.png";
 
 export default function HeroSection(): React.JSX.Element {
-  return <section id="hero"></section>;
+  return (
+    <section id="hero" className={style.section_container}>
+      <div className={style.left}>
+        <div className={style.text_wrapper}>
+          <h1 className={style.hero_title}>
+            Everything you need to scale a{" "}
+            <span className={style.highlight_txt}>global team</span>
+          </h1>
+          <p className={style.hero_txt}>
+            Check out our proven methods, guides, and exercises that help works
+            better and people happier.
+          </p>
+        </div>
+      </div>
+      <div className={style.right}>
+        <img src={heroImg} alt="Hero section background image" />
+      </div>
+    </section>
+  );
 }
